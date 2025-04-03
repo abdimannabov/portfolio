c0c47ee3daaa389e39ce1fba9f46e800fa48ef67
Sohib <abdumanobovsohib13@gmail.com>
2025-04-03 22:05:55 +0500

    Databaza yaratdim va detail page boshladim

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from portfolio.views import *
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("detail/", detail, name="detail")
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from portfolio.models import Project
 # Create your views here.
 def index(request):
-    return render(request, "portfolio/index.html")
+    projects = Project.objects.all()
+    context = {
+        "projects":projects,
+        "title":"Portfolio"
+    }
+    return render(request, "portfolio/index.html", context)
+
+def detail(request):
+    return render(request, "portfolio/detail.html")
